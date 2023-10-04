51a5d36349ec1b5e34be17912d259e666aa5c5f1
Interian Eduardo <48145738+Archi3213@users.noreply.github.com>
2023-10-03 19:34:04 -0600

    primer acomodo

--- art_galleryJS/index.js
+++
@@ -1,6 +0,0 @@
-/*
-Inicio del codigo 
-*/
-let titulo = "LE PETIT LOUVRE"
-
-console.log(titulo);
---
+++ art_galleryJS/script.js
@@ -0,0 +1,6 @@
+// Obtener una referencia al elemento body
+const body = document.body;
+
+// Cambiar el color de fondo del body
+body.style.backgroundColor = "gray";
+
