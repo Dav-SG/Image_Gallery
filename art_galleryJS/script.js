e0e46fcccef4fb34d51b9e66c07f2a6aca2f9078
Gustavo Herrera <guss_herrera_r@hotmail.com>
2023-10-05 04:24:09 -0600

    Se añade nuevamente la funcion de modo oscuro al index

--- art_galleryJS/script.js
+++ art_galleryJS/script.js
@@ -664,3 +664,32 @@ document.addEventListener('DOMContentLoaded', function (){
 
     document.head.appendChild(styles);
 });
+
+document.addEventListener('DOMContentLoaded', function (){
+
+    darkMode();
+});
+
+function darkMode(){
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+    if(prefiereDarkMode.matches){
+        document.body.classList.add('dark-mode');
+    } else{
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereDarkMode.addEventListener('change', function (){
+        if(prefiereDarkMode.matches){
+            document.body.classList.add('dark-mode');
+        } else{
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
+    const botonDarkMode = document.querySelector('.dark-mode-boton');
+
+    botonDarkMode.addEventListener('click', function (){
+        document.body.classList.toggle('dark-mode');
+    });
+}
