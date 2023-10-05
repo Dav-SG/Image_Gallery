2090a9aab94f3ab867d86f78068ac5f87dbdee51
Gherrera99 <guss_herrera_r@hotmail.com>
2023-10-04 23:04:30 -0600

    Se empieza a maquetar el header con el primer divWrapper que contiene el titulo de la pagina

--- art_galleryJS/script.js
+++ art_galleryJS/script.js
@@ -1,6 +1,29 @@
-// Obtener una referencia al elemento body
-const body = document.body;
+// // Obtener una referencia al elemento body
+// const body = document.body;
+//
+// // Cambiar el color de fondo del body
+// body.style.backgroundColor = "gray";
 
-// Cambiar el color de fondo del body
-body.style.backgroundColor = "gray";
+// Creación del header
+const header = document.querySelector('header');
+const divWrapper = document.createElement('div');
+const divContainer = document.createElement('div');
+
+//Se añade el div "wrapper" al header
+divWrapper.className ='wrapper';
+header.appendChild(divWrapper);
+
+const svgElement = document.createElement('svg');
+svgElement.innerHTML = `
+  <text x="50%" y="50%" dy=".35em" text-anchor="middle">LE PETIT LOUVRE</text>
+`;
+
+const h1Element = document.createElement('h1');
+h1Element.textContent = 'Art Gallery///Galeria De Arte';
+
+divWrapper.appendChild(svgElement);
+divWrapper.appendChild(h1Element);
+
+
+
 
