a00adcfad09531f4b36af234f9395ed744d5615c
Gherrera99 <guss_herrera_r@hotmail.com>
2023-10-04 23:54:50 -0600

    Se añade el div container al header y se añade el 1er div dentro de container

--- art_galleryJS/script.js
+++ art_galleryJS/script.js
@@ -24,6 +24,39 @@ h1Element.textContent = 'Art Gallery///Galeria De Arte';
 divWrapper.appendChild(svgElement);
 divWrapper.appendChild(h1Element);
 
+//Se añade el div "container" al header
+divContainer.className = 'container';
+header.appendChild(divContainer);
 
+const imgHeader = document.createElement('img');
+imgHeader.src = './svg/dark-mode.svg';
+imgHeader.alt = 'icono dark mode';
+imgHeader.className = 'dark-mode-boton';
+
+const dropdown1 = document.createElement('div');
+dropdown1.className = 'dropdown';
+
+const link1 = document.createElement('a');
+link1.href = 'France.html';
+const btn1 = document.createElement('button');
+btn1.className = 'dropbtn';
+btn1.textContent = 'France';
+link1.appendChild(btn1);
+
+const dropdownContent1 = document.createElement('div');
+dropdownContent1.className = 'dropdown-content';
+const links1 = ['Claude_Monet.html', 'Georges_Braque.html', 'Georges-Pierre_Seurat.html'];
+
+links1.forEach(link => {
+    const anchor = document.createElement('a');
+    anchor.href = link;
+    anchor.textContent = link.replace('.html', '');
+    dropdownContent1.appendChild(anchor);
+});
+
+dropdown1.appendChild(link1);
+dropdown1.appendChild(dropdownContent1);
+divContainer.appendChild(imgHeader);
+divContainer.appendChild(dropdown1);
 
 
