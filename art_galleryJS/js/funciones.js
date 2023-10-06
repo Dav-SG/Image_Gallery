49bb1abfc3792bac819e25823b842e1b78eb9d05
cisco p <55768594+FranciscoPG-69@users.noreply.github.com>
2023-10-06 15:02:07 -0600

    agregando en comentario validacion de formulario

--- art_galleryJS/js/funciones.js
+++ art_galleryJS/js/funciones.js
@@ -66,5 +66,31 @@ function darkMode(){
         document.body.classList.toggle('dark-mode');
     });
 }
+/*agregando validación al formulario
+const nombre = document.getElementById('name');
+const correo = document.getElementById('correoEntrada');
+const contra = document.getElementById('passwordEntrada');
+const botonRegis = document.getElementById('boton-re');
+const expresionesCorreo = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/; // Expresión regular para validar el correo electrónico
+
+b//las expresiones regulares se guardan en la variable y se usara para la validación del correo
 
 
+botonRegis.addEventListener("click", (event) =>{//se crea una función de flecha para el evento de click en el boton 
+    
+    
+    if (nombre.value ===  null || nombre.value === ''){//aqui validamos el nombre no este vacio ni tenga datos nulos
+        alert('Ingresa un nombre correcto')
+    }
+    if(contra.value ===null || contra.value=== ''){//se valida la contraseña que no tenga datos nuloos o este vacio
+        alert('Ingresa una contraseña correcta')
+    }
+    if(!expresionesCorreo.test(correo.value)){//se verifica que el correo con la ayuda de las expresiones regulares que cada cadena de valores valida diferentes secciones en el correo
+        alert('ingresa un correo valido')
+    }
+    else{
+
+    alert('Registrado');
+    }
+});
+*/
