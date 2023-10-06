23ca1de730b012de353a374264ee6259fb839482
cisco p <55768594+FranciscoPG-69@users.noreply.github.com>
2023-10-05 19:14:20 -0600

    Se agrego una funcionalidad de verificacion de formulario con js

--- art_galleryhtml/js/scripts.js
+++ art_galleryhtml/js/scripts.js
@@ -26,3 +26,31 @@ function darkMode(){
         document.body.classList.toggle('dark-mode');
     });
 }
+
+//Agregando una validación al formulario
+const nombre = document.getElementById('name');
+const correo = document.getElementById('Email');
+const contra = document.getElementById('password');
+const botonregis= document.getElementById('boton-re');
+const expresionesCorreo= /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+
+
+
+botonregis.addEventListener("click", (event) =>{
+    
+    
+    if (nombre.value ===  null || nombre.value === ''){
+        alert('Ingresa un nombre correcto')
+    }
+    if(contra.value ===null || contra.value=== ''){
+        alert('Ingresa una contraseña correcta')
+    }
+    if(!expresionesCorreo.test(correo.value)){
+        alert('ingresa un correo valido')
+    }
+    else{
+
+    alert('Registrado');
+    }
+});
+
